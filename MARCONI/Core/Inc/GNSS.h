13ea6aca0fafc6e7ea06d4237e7c14752cb3dcd7
Tommaso Gualtierotti <gualtierotti.tommaso@gmail.com>
2024-04-29 22:24:34 +0200

    chore: add baud rate and message list enums
    - add baud rate enum for choosing nmea port baud rate.
    - add message list enums for choosing the messages we want to receive from the gnss module.

--- MARCONI/Core/Inc/GNSS.h
+++ MARCONI/Core/Inc/GNSS.h
@@ -21,20 +21,58 @@
 #define GPS_WARM_START				"$PSTMWARM"
 #define GPS_HOT_START				"$PSTMHOT"
 #define GPS_SYS_RESET				"$PSTMSRR"
-#define GPS_ENGINE_RESET				"$PSTMGPSRESET"
+#define GPS_ENGINE_RESET			"$PSTMGPSRESET"
 #define GPS_ENGINE_SUSPEND			"$PSTMGPSSUSPEND"
 #define GPS_ENGINE_RESTART			"$PSTMGPSRESTART"
 
 #define SET_SYSTEM_PARAMETERS		"$PSTMSETPAR"
 #define GET_SYSTEM_PARAMETERS		"$PSTMGETPART"
 #define SAVE_SYSTEM_PARAMETERS		"$PSTMSAVEPAR"
+
 #define RESTORE_SYSTEM_PARAMETERS	"$PSTMRESTOREPAR"
 #define SEND_NMEA_MESSAGES			"$PSTMNMEAREQUEST"
 
 #define RF_TEST_MODE_ON				"$PSTMRFTESTON"
-#define RF_TEST_MODE_OFF				"$PSTMRFTESTOFF"
+#define RF_TEST_MODE_OFF			"$PSTMRFTESTOFF"
 
-//XXX /* page 181 for NMEA Port Baudrate settings */
+//XXX /* page 181 for NMEA Port Baudrate settings - CDB-ID 102 */
+//XXX /* page 190 for NMEA messages list - CDB-ID 201-228 */
+//XXX /* page 182 for NMEA Msg-List 0 output rate scaling factor - CDB-ID 190 */
+//XXX /* page 185 for fix-rate GNSS value - CDB-ID 303 */
+
+/* page 8, 71 for parameters changing */
+/* to actually change a parameter:
+ * $PSTMSETPAR		changes the parameter
+ * $PSTMSAVEPAR		saves the configuration
+ * $PSTMSRR			resets the module to make changes effective
+ */
+
+typedef enum {
+	BAUD_RATE_300 = 0x0,
+	BAUD_RATE_600 = 0x1,
+	BAUD_RATE_1200 = 0x2,
+	BAUD_RATE_2400 = 0x3,
+	BAUD_RATE_4800 = 0x4,
+	BAUD_RATE_9600 = 0x5,
+	BAUD_RATE_14400 = 0x6,
+	BAUD_RATE_19200 = 0x7,
+	BAUD_RATE_38400 = 0x8,
+	BAUD_RATE_57600 = 0x9,
+	BAUD_RATE_115200 = 0xA,	/* DEFAULT */
+	BAUD_RATE_230400 = 0xB,
+	BAUD_RATE_460800 = 0xC,
+	BAUD_RATE_921600 = 0xD
+} GNSS_NMEA_BAUD_RATE;
+
+typedef enum {
+	GPGNS = 0x1,
+	GPGGA = 0x2,
+	GPGSA = 0x4,
+	GPRMC = 0x40,
+	GPGSV = 0x80000,
+	GPGLL = 0x100000,
+	GPZDA = 0x1000000
+} NMEA_MESSAGE_LIST;
 
 
 /* STANDARD NMEA MESSAGE TALKER ID */
@@ -50,13 +88,15 @@ typedef struct {
 	UART_HandleTypeDef *handler;
 
 	/* do we need to put string termination character \0 ? */
-	uint8_t utc_time[9];
-	uint8_t latitude[7];
+	uint8_t utc_time[11];
+	uint8_t latitude[11];
 	uint8_t latitude_dir;
-	uint8_t longitude[7];
+	uint8_t longitude[11];
 	uint8_t longitude_dir;
-	uint8_t altitude[6];
+	uint8_t altitude[7];
 	uint8_t altitude_meas_unit;
+
+	/* add write and read function */
 } gnss_t;
 
 typedef struct {
@@ -76,5 +116,11 @@ typedef struct {
 uint8_t gnss_init(gnss_t *dev, UART_HandleTypeDef *huart);
 
 uint8_t gnss_set_init_position_time(gnss_t *dev, config_params_t *params);
+
+uint8_t gnss_change_baud_rate(gnss_t *dev, GNSS_NMEA_BAUD_RATE rate);
+
+uint8_t gnss_set_nmea_messages(gnss_t *dev, uint32_t msg_mask);
+
+uint8_t parse_gga_message(gnss_t *dev, uint8_t *msg);
 
 #endif /* GNSS_H */
