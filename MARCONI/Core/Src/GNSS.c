4abe5c6ec286948875feda047e228b3e90892a89
Tommaso Gualtierotti <gualtierotti.tommaso@gmail.com>
2024-04-29 22:25:42 +0200

    feat: add fn to parse GGA message

--- MARCONI/Core/Src/GNSS.c
+++ MARCONI/Core/Src/GNSS.c
@@ -20,8 +20,37 @@ uint8_t gnss_set_init_position_time(gnss_t *dev, config_params_t *params) {
 
 	uint8_t result = HAL_ERROR;
 
-	uint8_t buf[60] = {0};	/* check correctly the right size of the message */
+	uint8_t buf[65] = {0};
+
+
 
 	return result;
 }
 
+uint8_t parse_gga_message(gnss_t *dev, uint8_t *msg) {
+
+	uint8_t result = HAL_ERROR;
+
+	/* $GPGGA,<Timestamp>,<Lat>,<N/S>,<Long>,<E/W>,<GPSQual>,<Sats>,<HDOP>,<Alt>,<AltVal>,
+	 * <GeoSep>,<GeoVal>,<DGPSAge>,<DGPSRef>*<checksum><cr><lf> */
+
+	if (strncmp(msg, GGA, strlen(GGA)) != 0) {
+		return result;
+	} else {
+		result = HAL_OK;
+	}
+
+	//XXX
+	/* still to understand whether the latitude and longitude return a string of 10 characters or 11, in the case below
+	 * 10 character is assumed */
+
+	memcpy(dev -> utc_time, &msg[7], 10);	/* copy the time in the related variable */
+	memcpy(dev -> latitude, &msg[18], 10);
+	dev -> latitude_dir = msg[29];
+	memcpy(dev -> longitude, &msg[31], 10);
+	dev -> longitude_dir = msg[42];
+	memcpy(dev -> altitude, &msg[54], 6);
+	dev -> altitude_meas_unit = msg[61];
+
+	return result;
+}
